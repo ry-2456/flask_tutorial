65cb28bdafc75ceb57c9c6c8c0e6836856de41c1
ry-2456 <ittanmomennimakaretyaimashita@gmail.com>
2020-05-15 12:39:45 +0900

    add: first commit in flask_tutorial
    いろいろなファイルを新規追加した

---
+++ flaskr/__init__.py
@@ -0,0 +1,11 @@
+from flask import Flask
+# from flask.ext.sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config.from_object('flaskr.config')
+
+db = SQLAlchemy(app)
+
+# import flaskr.views
+from flaskr import views
---
+++ manage.py
@@ -0,0 +1,3 @@
+from flaskr import app
+
+app.run(host="192.168.100.103", port=5000, debug=true)
